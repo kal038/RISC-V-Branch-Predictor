bfbed38a35620d50a746208c441b759b12dcc89e
ks061 <ks061@bucknell.edu>
2021-11-28 15:44:44 -0500

    Added branch grouping dictionary list to BranchPredictor class

--- branch_predictor.py
+++ branch_predictor.py
@@ -14,12 +14,17 @@ class BranchPredictor:
     instr_filepath = None
     instr_file = None
     instr_dict_list = None
+    
+    grouped_branch_seqs = None
 
     def init():
         BranchPredictor.instr_filepath = sys.argv[1]
         BranchPredictor.instr_file = open(BranchPredictor.instr_filepath)
         BranchPredictor.instr_dict_list = []
         BranchPredictor._import_instrs()
+
+        BranchPredictor.grouped_branch_seqs = []
+        BranchPredictor._group_branch_seqs()
 
     def _import_instrs():
         lines = BranchPredictor.instr_file.readlines()
@@ -51,18 +56,49 @@ class BranchPredictor:
             BranchPredictor._grow_instr_dict_list(instr_num)
             BranchPredictor.instr_dict_list[instr_num] = instr_dict
 
+    def _group_branch_seqs():
+        for i in range(len(BranchPredictor.instr_dict_list)):
+            if BranchPredictor.instr_dict_list[i] == None: continue
+            instr_str = BranchPredictor.instr_dict_list[i]["str"]
+            if instr_str.split()[0][0] == "b":
+                regex = '([a-fa-f0-9]{8})'
+                hex_vals = re.findall(regex, instr_str)
+                if hex_vals == None: continue
+                if len(hex_vals) < 1: continue
+                predicted_pc = hex_vals[-1]           
+                actual_pc = BranchPredictor.instr_dict_list[i+1]["pc"]
+                is_taken = predicted_pc == actual_pc
+                branch_seq_dict = {"instr": BranchPredictor.instr_dict_list[i],
+                                   "predicted_pc": predicted_pc,
+                                   "actual_pc": actual_pc,
+                                   "is_taken": is_taken
+                                   }
+                BranchPredictor.grouped_branch_seqs.append(branch_seq_dict)
+
+    def _get_dict_list_str(dict_list: list):
+        output = "[\n"
+        for i in range(len(dict_list)):
+            output += f"{i}: {dict_list[i]}"
+            if i != len(BranchPredictor.instr_dict_list)-1:
+                output += ",\n"
+        output += "\n]"
+        return output
+        
+
+    def get_str():
+        output = "Instructions:\n"
+        output += BranchPredictor._get_dict_list_str(
+            BranchPredictor.instr_dict_list
+        )
+        output += "\n\nGrouped Branch Sequences\n"
+        output += BranchPredictor._get_dict_list_str(
+            BranchPredictor.grouped_branch_seqs
+        )
+        return output
+    
     def _grow_instr_dict_list(instr_num: int):
         if len(BranchPredictor.instr_dict_list) < 1:
             BranchPredictor.instr_dict_list.append(None)
         curr_instr_dict_len = len(BranchPredictor.instr_dict_list)
         for i in range(instr_num - curr_instr_dict_len + 2):
             BranchPredictor.instr_dict_list.append(None)
-
-    def get_str():
-        output = "[\n"
-        for i in range(len(BranchPredictor.instr_dict_list)):
-            output += f"{i}: {BranchPredictor.instr_dict_list[i]}"
-            if i != len(BranchPredictor.instr_dict_list)-1:
-                output += ",\n"
-        output += "\n]"
-        return output
