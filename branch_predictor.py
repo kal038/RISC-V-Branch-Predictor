e798559cc8b8432817bfde7913a088c204a2f045
ks061 <ks061@bucknell.edu>
2021-11-28 15:11:32 -0500

    Building BranchPredictor class functionality, including printing instructions effectively

--- branch_predictor.py
+++ branch_predictor.py
@@ -11,42 +11,58 @@ import sys
 
 class BranchPredictor:
 
-	instr_filepath = None
-	instr_file = None
-	instr_dict_list = None
+    instr_filepath = None
+    instr_file = None
+    instr_dict_list = None
 
-	def init():
-		BranchPredictor.instr_filepath = sys.argv[1]
+    def init():
+        BranchPredictor.instr_filepath = sys.argv[1]
+        BranchPredictor.instr_file = open(BranchPredictor.instr_filepath)
+        BranchPredictor.instr_dict_list = []
+        BranchPredictor._import_instrs()
 
-		# Adapted from
-		BranchPredictor.instr_file = open(BranchPredictor.instr_filepath)
-		BranchPredictor.instr_dict_list = []
-		lines = BranchPredictor.instr_file.readlines()
-		for line in lines:
-			regex = '([a-fa-f0-9]{8})'
-			hex_vals = re.findall(regex, line)
-			if hex_vals == None: continue
-			if len(hex_vals) < 2: continue
-			pc = hex_vals[0]
-			ir = hex_vals[1]
-	
-			regex = '^(.+?):'
-			entity_pre_colon_list = re.findall(regex, line)
-			if entity_pre_colon_list  == None: continue
-			if len(entity_pre_colon_list) < 1: continue
-			instr_num = entity_pre_colon_list[0]
+    def _import_instrs():
+        lines = BranchPredictor.instr_file.readlines()
+        for line in lines:
+            regex = '([a-fa-f0-9]{8})'
+            hex_vals = re.findall(regex, line)
+            if hex_vals == None: continue
+            if len(hex_vals) < 2: continue
+            pc = hex_vals[0]
+            ir = hex_vals[1]
 
-			regex = '.*?, .*?, (.+)'
-			instr_str_list = re.findall(regex, line)
-			if instr_str_list == None: continue
-			if len(instr_str_list) < 1: continue
-			instr_str = instr_str_list[0]
+            regex = '^(.+?):'
+            entity_pre_colon_list = re.findall(regex, line)
+            if entity_pre_colon_list  == None: continue
+            if len(entity_pre_colon_list) < 1: continue
+            instr_num = int(entity_pre_colon_list[0])
 
-			instr_dict = {"pc": pc,
-					"ir": ir,
-					"str": instr_str
-			}
+            regex = '.*?, .*?, (.+)'
+            instr_str_list = re.findall(regex, line)
+            if instr_str_list == None: continue
+            if len(instr_str_list) < 1: continue
+            instr_str = instr_str_list[0]
 
-			instr_dict[instr_num] = instr_dict
+            instr_dict = {"pc": pc,
+                        "ir": ir,
+                        "str": instr_str
+            }
 
-			print(instr_dict)
+            BranchPredictor._grow_instr_dict_list(instr_num)
+            BranchPredictor.instr_dict_list[instr_num] = instr_dict
+
+    def _grow_instr_dict_list(instr_num: int):
+        if len(BranchPredictor.instr_dict_list) < 1:
+            BranchPredictor.instr_dict_list.append(None)
+        curr_instr_dict_len = len(BranchPredictor.instr_dict_list)
+        for i in range(instr_num - curr_instr_dict_len + 2):
+            BranchPredictor.instr_dict_list.append(None)
+
+    def get_str():
+        output = "[\n"
+        for i in range(len(BranchPredictor.instr_dict_list)):
+            output += f"{i}: {BranchPredictor.instr_dict_list[i]}"
+            if i != len(BranchPredictor.instr_dict_list)-1:
+                output += ",\n"
+        output += "\n]"
+        return output
--- branch_predictor_runner.py
+++ branch_predictor_runner.py
@@ -2,3 +2,4 @@ from branch_predictor import BranchPredictor
 
 if __name__ == "__main__":
 	BranchPredictor.init()
+	print(BranchPredictor.get_str())
